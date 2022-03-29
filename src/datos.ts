dd4054d13983441a45b0ba1f385d0b6c477b6e6b
VaqueroIvan <102192287+VaqueroIvan@users.noreply.github.com>
2022-03-28 21:44:47 -0300

    REV01

---
+++ src/datos.ts
@@ -0,0 +1,26 @@
+// //Primer programa clase 1 Carla
+// let h: number = 5;
+// let l: number = 10;
+// let minombre: string = "";
+// let area: number = h * l;
+// const gato: boolean = true;
+// minombre = "Ivan";
+// console.log("El area es: " + area);
+// console.log("Mi nombre es: " + minombre);
+// console.log("gato: " gato)
+// alert("El area es: " + area);
+// let btnEnviar = <HTMLButtonElement>document.getElementById("btnEnviar");
+// let dato = <HTMLInputElement>document.getElementById("dato");
+// btnEnviar.addEventListener("click", () => {
+//   //acá va nuestro codigo.
+//   console.log("dato " + dato.Value);
+//   console.log("rotulo " + rotulo.nodeValue);
+// });
+let btnEnviar = <HTMLButtonElement>document.getElementById("btnEnviar");
+let dato = <HTMLInputElement>document.getElementById("dato");
+let nombre: string;
+
+btnEnviar.addEventListener("click", () => {
+  // Acá va nuestro código
+  console.log("dato:", dato.value);
+});
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-var h = 5;
-var l = 10;
-var area = h * l;
-console.log("el area es: " + area);
